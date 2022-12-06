b6587be369579f7d725f7588f1d6c2a732cf9884
Ferza Noveri <110340182+ferzanoveri@users.noreply.github.com>
2022-12-06 16:42:15 +0700

    Add files via upload

---
+++ SnakeGame/src/snakegame/SnakeGame.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package snakegame;
+
+/**
+ *
+ * @author ferzanoveri
+ */
+public class SnakeGame {
+  
+    public static void main(String[] args) {
+        
+        GameFrame frame = new GameFrame();
+        
+    }
+    
+}
